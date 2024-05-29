521b2df3d9044859f050e19f7444dbc5d159a1f6
nanramirez <nan.php.mx@gmail.com>
2024-05-29 14:14:33 -0600

    jump x

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private bool _grounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput= Input.GetAxis("Horizontal"); 
-        _rigid.velocity= new Vector2(horizontalInput, _rigid.velocity.y);
+        float move= Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true) {
+        _rigid.velocity=new Vector2(_rigid.velocity.x, -_jumpForce);
+            _grounded = false;
+        }
+        RaycastHit2D hitInfo=Physics2D.Raycast(transform.position, Vector2.down,1.0f);
+        if(hitInfo.collider != null ) { 
+            _grounded= true;
+        }
+        _rigid.velocity= new Vector2(move, _rigid.velocity.y);
     }
 }
