44b74fac2fa9074b6fc6c246fd15929362a82acc
nanramirez <nan.php.mx@gmail.com>
2024-05-31 20:02:43 -0600

    Improve Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,40 +6,51 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
     [SerializeField] private float _jumpForce = 5.0f;
-    [SerializeField] private bool _grounded = false;
+   
     [SerializeField] private LayerMask _groundLayer;
     private bool _resetJump=false;
     // Start is called before the first frame update
     void Start()
     {
+        
         _rigid= GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float move= Input.GetAxis("Horizontal");
-         if (Input.GetKeyDown(KeyCode.Space) && _grounded == true) {
-        _rigid.velocity=new Vector2(_rigid.velocity.x, _jumpForce);
-        
-            _grounded = false;
-             _resetJump=true;
-             StartCoroutine(ResetJumpRoutine());
-        }
-        RaycastHit2D hitInfo=Physics2D.Raycast(transform.position, Vector2.down,0.06f,1<<8);
-        Debug.DrawRay(transform.position, Vector2.down* 0.06f , Color.green);
-        if(hitInfo.collider != null ) {
-            Debug.Log("Hit: "+hitInfo.collider.name); 
-            if(_resetJump==false)
-                _grounded= true;
-           
-        }
+    void Update(){
+        Movement();
+       
+       
        
         
-        _rigid.velocity= new Vector2(move, _rigid.velocity.y);
+        
     }
-    IEnumerator ResetJumpRoutine(){
+   
+    void Movement(){
+         float move= Input.GetAxis("Horizontal");
+         _rigid.velocity= new Vector2(move, _rigid.velocity.y);
+         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true) {
+            _rigid.velocity=new Vector2(_rigid.velocity.x, _jumpForce);
+        
+             StartCoroutine(ResetJumpRoutine());
+        }
+        
+
+    }
+    bool IsGrounded(){
+         RaycastHit2D hitInfo=Physics2D.Raycast(transform.position, Vector2.down,0.06f,1<<8);
+        Debug.DrawRay(transform.position, Vector2.down* 0.06f , Color.green);
+        if(hitInfo.collider != null ) {
+            if(_resetJump==false)return true;
+           
+        }
+        return false;
+        
+
+    }
+     IEnumerator ResetJumpRoutine(){
+        _resetJump =true;
         yield return new WaitForSeconds(0.1f);
         _resetJump =false;
 
