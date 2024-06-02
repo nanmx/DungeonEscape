62a08cf6420cf312e1d5e1157271cd4413186303
nanramirez <nan.php.mx@gmail.com>
2024-06-01 23:59:21 -0600

    Player Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,16 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
     [SerializeField] private float _jumpForce = 5.0f;
-   
+    [SerializeField] private float _speed = 5.0f;
     [SerializeField] private LayerMask _groundLayer;
     private bool _resetJump=false;
+    private PlayerAnimation _playerAnimation;
     // Start is called before the first frame update
     void Start()
     {
         
         _rigid= GetComponent<Rigidbody2D>();
+        _playerAnimation= GetComponent<PlayerAnimation>();
         
     }
 
@@ -29,13 +31,14 @@ public class Player : MonoBehaviour
    
     void Movement(){
          float move= Input.GetAxis("Horizontal");
-         _rigid.velocity= new Vector2(move, _rigid.velocity.y);
+        
          if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true) {
             _rigid.velocity=new Vector2(_rigid.velocity.x, _jumpForce);
         
              StartCoroutine(ResetJumpRoutine());
         }
-        
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+        _playerAnimation.Move(move);
 
     }
     bool IsGrounded(){
